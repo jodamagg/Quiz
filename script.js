ee442dc6b53f75b55c2c26fe00e79ab8fd0e0897
unknown <jodamagg@gmail.com>
2021-04-27 22:10:30 -0600

    Created functions and event listeners for start button

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 // I need to make a start button
 //Button needs to be clickable
 //when button is clicked a question is presented
+//when we click start//
+var startButton = document.getElementById("start-btn");
+var questContEle = document.getElementById("question-container");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Started");
+  //hide the start button when clicked
+  startButton.classList.add("hide");
+
+  //show the questions when start button hidden
+  questContEle.classList.remove("hide");
+  setNextQuestion;
+}
+//when we click "next"
+function setNextQuestion() {}
+
+function selectAnswer() {}
+
+var questions = [
+  {
+    question: "A boolean's value can only be",
+    answers: [
+      { text: "True or False", correct: true },
+      { text: "A string", correct: false },
+      { text: "A number", correct: false },
+      { text: "None of the above", correct: false },
+    ],
+  },
+];
